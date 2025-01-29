11f25e3d7543789f9315e8404a9495327970de82
Ritwik Gupta <ritwikg2004@live.com>
2025-01-29 11:05:03 -0500

    Gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), partytown()],
     markdown: {
         remarkPlugins: [remarkMath],
         rehypePlugins: [[rehypeKatex, { output: "mathml" }]],
